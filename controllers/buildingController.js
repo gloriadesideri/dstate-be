d8fa9704bd090b82435738d07cbff36edf2d9eb6
Gloria Desideri <gloriadesideri00@gmail.com>
2022-05-23 14:00:51 +0200

    added routes to buy and sell tokens

--- controllers/buildingController.js
+++ controllers/buildingController.js
@@ -6,6 +6,7 @@ const Token = require('../models/Token')
 const Building = require ('../models/Building')
 const ipfsClient = require ('ipfs-http-client');
 const { transformAuthInfo } = require('passport');
+const {approveToken} = require("./buildingController");
 const ipfs= ipfsClient.create('https://ipfs.infura.io:5001/api/v0')
 const walletAPIUrl = 'https://rinkeby.infura.io/v3/2af9187666bc4f2485d90c76f9727138';
 
@@ -14,8 +15,6 @@ const web3 = new Web3(walletAPIUrl);
 exports.getEncodedABI = async (req, res, next) => {
 
     const pathToFile=path.join(__dirname,'../solidity/build/contracts','NewToken.json')
-
-
     var data = JSON.parse(fs.readFileSync(pathToFile));
     var myContract = new web3.eth.Contract(data.abi);
     let encodedABI=await myContract.deploy({
@@ -28,15 +27,24 @@ exports.getEncodedABI = async (req, res, next) => {
 exports.createSetPriceTransaction = async (req,res, next)=>{
     const pathToFile=path.join(__dirname,'../solidity/build/contracts','BuySell.json')
     var data = JSON.parse(fs.readFileSync(pathToFile));
+    const pathToTokenFile=path.join(__dirname,'../solidity/build/contracts','NewToken.json')
+    var TokenData = JSON.parse(fs.readFileSync(pathToTokenFile));
     var myContract = new web3.eth.Contract(data.abi, process.env.BUY_SEELL_ADDRESS);
-    let encodedABI=await myContract.methods.setPrice(req.body.amountOfETH*Math.pow(10, 18),req.body.tokenAmount*Math.pow(10, 18),req.body.tokenAddress).encodeABI()
+    var tokenContract = new web3.eth.Contract(TokenData.abi, req.body.tokenAddress);
+    const allowance = await tokenContract.methods.allowance(req.user.publicAddress,process.env.BUY_SEELL_ADDRESS)
+    var remaining = BigInt(allowance.remaining)
+
+    if(remaining<BigInt(req.body.tokenAmount*Math.pow(10, 18))){
+        await approveToken(req, res)
+    }
+    let encodedABI=await myContract.methods.setPrice(BigInt(req.body.amountOfETH*Math.pow(10, 18)),BigInt(req.body.tokenAmount*Math.pow(10, 18)),req.body.tokenAddress).encodeABI()
     res.send({abi:encodedABI})
 }
 exports.getPriceForTokens= async (req,res,next)=>{
     const pathToFile=path.join(__dirname,'../solidity/build/contracts','BuySell.json')
     var data = JSON.parse(fs.readFileSync(pathToFile));
     var myContract = new web3.eth.Contract(data.abi, process.env.BUY_SEELL_ADDRESS);
-    const res_from_get_price = await myContract.methods.getPriceForTokens(req.body.tokenAmount*Math.pow(10, 18),req.body.tokenAddress).call({from: req.user.publicAddress})
+    const res_from_get_price = await myContract.methods.getPriceForTokens(BigInt(req.body.tokenAmount*Math.pow(10, 18)),req.body.tokenAddress).call({from: req.user.publicAddress})
     console.log(res_from_get_price);
     res.send({price: res_from_get_price})
 }
@@ -44,7 +52,7 @@ exports.createBuyTokenTransaction = async (req,res,next)=>{
     const pathToFile=path.join(__dirname,'../solidity/build/contracts','BuySell.json')
     var data = JSON.parse(fs.readFileSync(pathToFile));
     var myContract = new web3.eth.Contract(data.abi, process.env.BUY_SEELL_ADDRESS);
-    var encodedABI= await myContract.methods.buyTokens(req.body.tokenAddress, req.body.promisedPrice*Math.pow(10, 18),req.body.tokenAmount*Math.pow(10, 18) ).encodeABI();
+    var encodedABI= await myContract.methods.buyTokens(req.body.tokenAddress, BigInt(req.body.promisedPrice*Math.pow(10, 18)),BigInt(req.body.tokenAmount*Math.pow(10, 18)) ).encodeABI();
     res.send({abi:encodedABI})
 }
 exports.createToken = async (req,res,next)=>{
@@ -110,11 +118,21 @@ exports.approveBuilding = async (req, res)=>{
 exports.fetchBuildings = async (req,res)=>{
     let buildings;
     if(req.params.building_id){
-        let building = await Building.findOne({id:req.params.building_id});
+        let building = await Building.findOne({id:req.params.building_id}).populate("tokens", "-__v").select("-__v");
         buildings.push(building)
     }else{
-        buildings = await Building.find({user_id:req.user._id});
+        buildings = await Building.find({user_id:req.user._id}).populate("tokens", "-__v").select("-__v");
     }
     res.send(200, {buildings})
 }
+exports.approveToken = async (req,res)=>{
+    const pathToTokenFile=path.join(__dirname,'../solidity/build/contracts','NewToken.json')
+    var TokenData = JSON.parse(fs.readFileSync(pathToTokenFile));
+    const building = await Building.findOne({_id:req.body.building_id}).populate("tokens", "-__v").select("-__v");
+    var myContract = new web3.eth.Contract(TokenData.abi, building.token.address);
+    var encodedABI= await myContract.methods.approve(process.env.BUY_SEELL_ADDRESS, BigInt(Math.pow(10,60))).encodeABI();
+    res.send({abi:encodedABI})
+}
+
+
 
--- routes/buildings.js
+++ routes/buildings.js
@@ -14,4 +14,12 @@ router.get("/unapproved", passport.authenticate("jwt",{session: false}), middlew
 router.post("/approve", passport.authenticate("jwt",{session: false}), middlewares.checkAdminRole, buildingController.approveBuilding)
 router.get("/", passport.authenticate("jwt",{session: false}), middlewares.checkAdminRole, buildingController.fetchBuildings)
 router.get("/approved",passport.authenticate("jwt",{session: false}), buildingController.fetchApproved)
+
+router.post("/createSetPriceTransaction", passport.authenticate("jwt",{session: false}), middlewares.checkForBuildingApproval, buildingController.createSetPriceTransaction)
+router.post("/getPriceForTokens", passport.authenticate("jwt",{session: false}), middlewares.checkForBuildingApproval, buildingController.getPriceForTokens)
+router.post("/createBuyTokenTransaction", passport.authenticate("jwt",{session: false}), middlewares.checkForBuildingApproval, buildingController.createBuyTokenTransaction)
+router.post("/approveToken", passport.authenticate("jwt",{session: false}), middlewares.checkForBuildingApproval, buildingController.approveToken)
+
+
+
 module.exports = router;
