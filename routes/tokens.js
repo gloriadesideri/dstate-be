29bc7b225c0404dea1daae33faa9422f02470fa9
Gloria Desideri <gloriadesideri00@gmail.com>
2022-05-26 10:50:37 +0200

    fixed balance of tokens route

--- routes/tokens.js
+++ routes/tokens.js
@@ -2,7 +2,6 @@ const passport = require("passport");
 const tokenController = require("../controllers/tokenController");
 var express = require('express');
 var router = express.Router();
-const passport = require('passport');
 const middlewares = require("../handlers/middlewares")
 // route to get the balance of a given token address
 router.get("/balanceOf",passport.authenticate("jwt",{session: false}), tokenController.balanceOfTokens );
